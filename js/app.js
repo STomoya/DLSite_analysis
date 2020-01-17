db4298fd50218a640de450ce205944dd12a421cd
STomoya <blackie0110@gmail.com>
2020-01-17 14:42:41 +0900

    [ADD] plot actor by tag

--- js/app.js
+++ js/app.js
@@ -2,16 +2,22 @@ app = new Vue({
     el: '#app',
     data: {
         selected_actor: 'そらまめ。',
-        labels: basic_data["labels"],
-        colors: basic_data["colors"],
+        selected_tag: 'バイノーラル/ダミヘ',
+        labels: basic_data['labels'],
+        colors: basic_data['colors'],
         actors: basic_data['actors'],
         full_data: full_data,
-        tag_chart: null
+        tag_chart: null,
+        actor_chart: null
     },
     methods: {
-        update_chart: function () {
+        update_tag_chart: function () {
             this.tag_chart.destroy()
-            this.create_chart()
+            this.create_tag_chart()
+        },
+        update_actor_chart: function () {
+            this.actor_chart.destroy()
+            this.create_actor_chart()
         },
         argsort: function (array) {
             const arrayObject = array.map((value, idx) => { return { value, idx }; });
@@ -27,7 +33,7 @@ app = new Vue({
             const argIndices = arrayObject.map(data => data.idx);
             return argIndices;
         },
-        sortList: function (dataList){
+        sortList: function (dataList, labelList){
             argIndices = this.argsort(dataList)
 
             var sorted_dataList = []
@@ -35,15 +41,18 @@ app = new Vue({
 
             for (let index in argIndices){
                 sorted_dataList.push(dataList[argIndices[index]]);
-                sorted_labelList.push(this.labels[argIndices[index]]);
+                sorted_labelList.push(labelList[argIndices[index]]);
             }
 
             return [sorted_dataList, sorted_labelList];
         },
-        create_chart: function () {
-            var dataList = this.full_data[this.selected_actor]
-
-            sorted_data = this.sortList(dataList)
+        create_tag_chart: function () {
+            if (!this.actors.includes(this.selected_actor)){
+                sorted_data = [[], []]
+            }else{
+                var dataList = this.full_data[this.selected_actor]
+                sorted_data = this.sortList(dataList, this.labels)
+            }
 
             config = {
                 type: 'pie',
@@ -68,9 +77,52 @@ app = new Vue({
                 }
             };
             this.tag_chart = new Chart(document.getElementById('tag-chart').getContext('2d'), config);
+        },
+        create_actor_chart_dataList: function (){
+            var index = this.labels.indexOf(this.selected_tag)
+
+            var dataList = []
+            for (let actor in this.full_data) {
+                dataList.push(this.full_data[actor][index], )
+            }
+
+            return dataList
+        },
+        create_actor_chart: function () {
+            if (!this.labels.includes(this.selected_tag)){
+                sorted_data = [[], []]
+            }else{
+                var dataList = this.create_actor_chart_dataList()
+                sorted_data = this.sortList(dataList, this.actors)
+            }
+
+            config = {
+                type: 'pie',
+                data: {
+                    labels: sorted_data[1],
+                    datasets: [{
+                        backgroundColor: this.colors,
+                        data: sorted_data[0],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: 'あるタグがついた作品への出演合計  (2020.01.15データ入手)',
+                        fontSize: 18
+                    },
+                    legend: {
+                        display: false
+                    },
+                    maintainAspectRatio: false
+                }
+            };
+            this.actor_chart = new Chart(document.getElementById('actor-chart').getContext('2d'), config);
         }
     },
     mounted: function () {
-        this.create_chart()
+        this.create_actor_chart();
+        this.create_tag_chart();
     }
 })
